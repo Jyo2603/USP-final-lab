e0b82dce292387d0f64bbb5201872beaed02042c
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2025-06-29 11:30:45 +0530

    Create 03a-remove_empty_files.c

---
+++ 03a-remove_empty_files.c
@@ -0,0 +1,21 @@
+#include <dirent.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    DIR *d = opendir(".");
+    struct dirent *e;
+    struct stat s;
+
+    while ((e = readdir(d))) {
+        stat(e->d_name, &s);
+
+        if (S_ISREG(s.st_mode) && s.st_size == 0) {
+            printf("Removed: %s\n", e->d_name);
+            remove(e->d_name);
+        }
+    }
+
+    closedir(d);
+}
