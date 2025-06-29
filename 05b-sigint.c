d9eaa7a76e9f443b4d19fe80677e3c00a817bbaa
Jyotsna <138552575+Jyo2603@users.noreply.github.com>
2025-06-29 17:09:02 +0530

    Create 05b-sigint.c

---
+++ 05b-sigint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void h(int s) {
+    puts("Ctrl+C pressed");
+    signal(SIGINT, SIG_DFL);
+}
+
+int main() {
+    struct sigaction sa = {.sa_handler = h};
+    sigaction(SIGINT, &sa, 0);
+
+    while (1) {
+        puts("Press Ctrl+C...");
+        pause();
+    }
+}
